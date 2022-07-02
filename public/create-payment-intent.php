8eda15de41adb967c80d681906d707ba1965e28f
smart715 <smartmanage715@gmail.com>
2022-07-02 19:15:38 +0900

    php webhook

---
+++ public/create-payment-intent.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+$output = [
+	'publishableKey' => "pk_test_51LGUiiLEUtRIzxdnBAD3lW5u0jxlXs2LMFDQbkXnvYurqdg1BiZlFkTccMwEHtJsiHFhSZV6E0ODxLTNhevop5pW00jUiNlfFX",
+
+
+	'clientSecret' => "pi_3LGpkwLEUtRIzxdn0HCl6ruw_secret_Q2tEpEGLUlwOnbJQVCbxBDUMu",
+];
+
+echo json_encode($output);
---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6b74eaed0f0c5a1b104d3933036560c1bbd3ee08',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6b74eaed0f0c5a1b104d3933036560c1bbd3ee08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.128.0',
+            'version' => '7.128.0.0',
+            'reference' => 'c704949c49b72985c76cc61063aa26fefbd2724e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
